fb76ce363fa636a0d6652b177d31763d0844439e
fghjjkl32 <fghjjkl32@naver.com>
2021-08-05 23:56:04 +0900

    feat | add : magic 8 ball  Level 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(BallHome());
+}
+
+class BallHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me AnyThing'),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: DicePage(),
       ),
     );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  // const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
